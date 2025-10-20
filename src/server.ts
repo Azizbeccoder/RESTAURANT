10ab81420e78a00f7e6e5fce50cab01c22062a3e
bektechie <https://github.com/bektechie>
2025-10-20 13:49:19 +0500

    feat ; integration of new enviromental variables

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("EXECUTED");
+//Architectural pattern ; MVC DI MVP;
+
+//MVC = MODEL VIEW CONTROLLER 
+
+
+//design pattern ; Middlewaare , Decotar
+
+import dotenv from "dotenv";
+dotenv.config();
+console.log("PORT",process.env.PORT);
+console.log("MONGO_URL",process.env.MONGO_URL);
